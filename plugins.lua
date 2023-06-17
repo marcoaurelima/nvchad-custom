87a71f7e4094ec96f3df98fc060061918dea0d02
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2023-06-17 18:51:59 -0300

    Alteração do atalho de aceitar sujestão TabNine

--- plugins.lua
+++ plugins.lua
@@ -61,7 +61,8 @@ local plugins = {
     config = function()
       require("tabnine").setup {
         disable_auto_comment = true,
-        accept_keymap = "<Tab>",
+        --accept_keymap = "<Tab>",
+        accept_keymap = "<Right>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
