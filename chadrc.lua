2755b773e2dedce3ce75e79e4b61e1b402aefa80
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2023-06-17 18:13:22 -0300

    Configurando Tabnine

--- chadrc.lua
+++ chadrc.lua
@@ -18,9 +18,28 @@ M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
 M.ui = {
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    -- overriden_modules = nil,
+    overriden_modules = function()
+      local st_modules = require "nvchad_ui.statusline.default"
+      -- this is just default table of statusline modules
+
+      return {
+        mode = function()
+          return st_modules.mode() .. require("tabnine.status").status()
+          -- or just return "" to hide this module
+        end,
+      }
+    end,
+  },
+
   nvdash = {
-    load_on_startup = true
-  }
+    load_on_startup = true,
+  },
 }
 
 return M
