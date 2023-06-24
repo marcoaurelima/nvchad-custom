7621c51f84ee3f02cb36ceb8abcbd7b47791123a
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2023-06-24 16:05:41 -0300

    inserção de comentários

--- mappings.lua
+++ mappings.lua
@@ -4,24 +4,27 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>ll"] = {"<cmd> LazyGit <CR>", "LazyGit"},
 
-    ["<leader>flr"] = {"<cmd> FlutterRun <CR>", "Flutter Run"},
-    ["<leader>fle"] = {"<cmd> FlutterEmulators <CR>", "Flutter Emulators"},
-    ["<leader>fld"] = {"<cmd> FlutterDevices <CR>", "Flutter Devices"},
-    ["<leader>flRl"] = {"<cmd> FlutterReload <CR>", "Flutter Reload"},
-    ["<leader>flRt"] = {"<cmd> FlutterRestart <CR>", "Flutter Restart"},
-    ["<leader>flq"] = {"<cmd> FlutterQuit <CR>", "Flutter Quit"},
-    ["<leader>flD"] = {"<cmd> FlutterDetach <CR>", "Flutter Detach"},
-    ["<leader>flO"] = {"<cmd> FlutterOutlineToggle <CR>", "Flutter Outline Toggle"},
-    ["<leader>flo"] = {"<cmd> FlutterOutlineOpen <CR>", "Flutter Outline Open"},
-    ["<leader>flt"] = {"<cmd> FlutterDevTools <CR>", "Flutter Dev Tools"},
-    ["<leader>flT"] = {"<cmd> FlutterDevToolsActivate <CR>", "Flutter Dev Tools Activate"},
-    ["<leader>flc"] = {"<cmd> FlutterCopyProfilerUrl <CR>", "Flutter Copy Profile Url"},
-    ["<leader>fll"] = {"<cmd> FlutterLspRestart <CR>", "Flutter LSP Restart"},
-    ["<leader>fls"] = {"<cmd> FlutterSuper <CR>", "Flutter Super"},
-    ["<leader>fly"] = {"<cmd> FlutterReanalyze <CR>", "Flutter Reanalyze"},
-    ["<leader>fln"] = {"<cmd> FlutterRename <CR>", "Flutter Rename"},
+    -- Lazy Git
+    ["<leader>ll"] = { "<cmd> LazyGit <CR>", "LazyGit" },
+
+    -- Flutter Development
+    ["<leader>flr"] = { "<cmd> FlutterRun <CR>", "Flutter Run" },
+    ["<leader>fle"] = { "<cmd> FlutterEmulators <CR>", "Flutter Emulators" },
+    ["<leader>fld"] = { "<cmd> FlutterDevices <CR>", "Flutter Devices" },
+    ["<leader>flRl"] = { "<cmd> FlutterReload <CR>", "Flutter Reload" },
+    ["<leader>flRt"] = { "<cmd> FlutterRestart <CR>", "Flutter Restart" },
+    ["<leader>flq"] = { "<cmd> FlutterQuit <CR>", "Flutter Quit" },
+    ["<leader>flD"] = { "<cmd> FlutterDetach <CR>", "Flutter Detach" },
+    ["<leader>flO"] = { "<cmd> FlutterOutlineToggle <CR>", "Flutter Outline Toggle" },
+    ["<leader>flo"] = { "<cmd> FlutterOutlineOpen <CR>", "Flutter Outline Open" },
+    ["<leader>flt"] = { "<cmd> FlutterDevTools <CR>", "Flutter Dev Tools" },
+    ["<leader>flT"] = { "<cmd> FlutterDevToolsActivate <CR>", "Flutter Dev Tools Activate" },
+    ["<leader>flc"] = { "<cmd> FlutterCopyProfilerUrl <CR>", "Flutter Copy Profile Url" },
+    ["<leader>fll"] = { "<cmd> FlutterLspRestart <CR>", "Flutter LSP Restart" },
+    ["<leader>fls"] = { "<cmd> FlutterSuper <CR>", "Flutter Super" },
+    ["<leader>fly"] = { "<cmd> FlutterReanalyze <CR>", "Flutter Reanalyze" },
+    ["<leader>fln"] = { "<cmd> FlutterRename <CR>", "Flutter Rename" },
   },
 }
 
