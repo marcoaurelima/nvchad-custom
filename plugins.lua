17c414a6718535aee8733c0d9b83fcfb20261105
Marco Aurélio <marco.aurelio@altislab.com.br>
2023-09-14 00:08:26 -0300

    retirando dashboard para melhorar a performance de inicio

--- plugins.lua
+++ plugins.lua
@@ -243,10 +243,6 @@ local plugins = {
     lazy = false,
   },
   {
-    "uga-rosa/ccc.nvim",
-    lazy = false,
-  },
-  {
     "utilyre/barbecue.nvim",
     lazy = false,
     name = "barbecue",
@@ -306,44 +302,6 @@ local plugins = {
   {
     "matze/vim-move",
     lazy = false,
-  },
-  {
-    "glepnir/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-      require("dashboard").setup {
-        theme = "hyper",
-        config = {
-          week_header = {
-            enable = true,
-          },
-          shortcut = {
-            { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-            {
-              icon = " ",
-              icon_hl = "@variable",
-              desc = "Files",
-              group = "Label",
-              action = "Telescope find_files",
-              key = "f",
-            },
-            {
-              desc = " Apps",
-              group = "DiagnosticHint",
-              action = "Telescope app",
-              key = "a",
-            },
-            {
-              desc = " dotfiles",
-              group = "Number",
-              action = "Telescope dotfiles",
-              key = "d",
-            },
-          },
-        },
-      }
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
   {
     "Shatur/neovim-session-manager",
